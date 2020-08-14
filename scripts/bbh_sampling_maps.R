33e3958653f790372ec83534f1d8235eeb7bc6ab
Jennifer Hoey <jahoey@users.noreply.github.com>
2020-08-13 19:41:34 -0700

    add alewife anadromous and landlocked populations

--- scripts/bbh_sampling_maps.R
+++ scripts/bbh_sampling_maps.R
@@ -2,10 +2,23 @@ library(maps)
 library(mapdata)
 library(rgdal)
 
+#### Read in data ####
 # Read in metadata for landlocked and baseline BBH
 meta_land <- read.table("~/Documents/UCSC_postdoc/blueback_herring/data/landlocked_metadata.txt")
 # meta_base <- read.delim("~/Documents/UCSC_postdoc/blueback_herring/data/baseline_metadata.txt", header = TRUE)
 anadromous_latlon <- read.delim("~/Documents/UCSC_postdoc/blueback_herring/maps/anadromous_latlon.txt", header = TRUE) # Many anadromous populations from Reid et al 2018, but not all
+
+# Read in metadata for landlocked and baseline ALE
+ale_base_latlon <- read.delim("~/Documents/UCSC_postdoc/alewife/data/ALE_anadromous_latlon.txt", header = TRUE, fileEncoding="latin1")
+ale_land_latlon <- read.delim("~/Documents/UCSC_postdoc/alewife/data/ALE_landlocked_latlon.txt", header = TRUE)
+
+# Which sites are shared by BBH and ALE?
+both <- intersect(ale_base_latlon$Location, anadromous_latlon$Code)
+
+# Make different data frames for BBH only, ALE only and both
+anadromous_latlon_only <- anadromous_latlon[!anadromous_latlon$Code %in% both,] # BBH only
+ale_base_latlon_only <- ale_base_latlon[!ale_base_latlon$Location %in% both,] # ALE only
+both_only <- ale_base_latlon[ale_base_latlon$Location %in% both,] # both BBH & ALE
 
 # Read in shapefiles with river and lake data
 us_rivers <- readOGR("~/Documents/UCSC_postdoc/blueback_herring/maps/ne_10m_rivers_lake_centerlines/ne_10m_rivers_lake_centerlines.shp")
@@ -18,7 +31,7 @@ H_0307Lakes <- readOGR("~/Documents/UCSC_postdoc/blueback_herring/maps/H_0307_la
 H_0305Lakes <- readOGR("~/Documents/UCSC_postdoc/blueback_herring/maps/H_0305_lakes/H_0305.shp")
 H_0601Lakes <- readOGR("~/Documents/UCSC_postdoc/blueback_herring/maps/H_0601_lakes/H_0601.shp")
 
-# Find unique lat/long combos
+# Find unique lat/long combos for BBH
 coors <- meta_land[!duplicated(meta_land[8:10]),]
 coors <- coors[-c(5,11:12),] # if lakes have multiple lat/lon, keep most common: Nottley (34.93285, -84.03903); Tugalo (34.75695, -83.31985)
 # coors_base <- meta_base[!duplicated(meta_base[5:7]),]
@@ -34,49 +47,78 @@ par(
   ps=14 # point size, which is the font size
 )
 
-map("worldHires", c("us", "canada"), xlim=c(-86,-60), ylim=c(28,48), col="gray92", fill=TRUE) #plots the region of the USA that I want
-map("state", xlim=c(-86,-60), ylim=c(28,48), add = TRUE, boundary=FALSE, col = 'gray70') # plots US state boundaries
+map("worldHires", c("us", "canada"), xlim=c(-90,-60), ylim=c(28,48), col="gray92", fill=TRUE) #plots the region of the USA that I want
+map("state", xlim=c(-90,-60), ylim=c(28,48), add = TRUE, boundary=FALSE, col = 'gray70') # plots US state boundaries
 plot(us_rivers, col='skyblue2', add=T) # plot big rivers
 plot(us_rivers_fine, col='skyblue2', add=T)
 title(xlab = "Longitude (°)", ylab = "Latitude (°)")
 
-axis(1, at=seq(-85,-60, by=5), labels=seq(-85,-60, by= 5))
+axis(1, at=seq(-90,-60, by=5), labels=seq(-90,-60, by= 5))
 axis(2, at=seq(28,48, by = 5), labels=seq(28,48, by= 5), las = TRUE)
 box()
 
-# Plot sampling locations
-rect(-84.5,34,-82.5,35.1, border = 'tomato', lwd = 1.5) # box around inset
+# Plot BBH sampling locations
+rect(-84.5,34,-82.5,35.1, border = 'black', lwd = 1.5) # box around inset
+rect(-73,40.7,-71.9,41.6, border = 'black', lwd = 1.5) # box around inset
 lines(c(-70.91866, -70), c(43.13086,43.3)) # lines first so the points are plotted on top
 lines(c(-70.94437, -70), c(42.98110,42.9))
 lines(c(-70.92917, -70), c(42.75007,42.4))
 lines(c(-69.5, -70.12213), c(41.7, 41.68207))
-lines(c(-70.62445,-70.6),c(41.73676,40.8))
+# lines(c(-70.62445,-70.6),c(41.73676,40.8))
 lines(c(-77.8, -76.8985), c(37.1,37.3727 ))
 lines(c(-78.1, -76.7903), c(37.55,37.5161 ))
-points(anadromous_latlon$Longitude, anadromous_latlon$Latitude, col = 'black', pch = 21, bg = 'tomato')
-points(coors$Longitude,coors$Latitude, col = 'black', pch = 21, bg = 'tomato')
+lines(c(-75.2, -76.13680), c(35.9, 35.69768))
+lines(c(-77.9, -76.67790), c(38.5, 38.58754))
+lines(c(-71.44630, -71), c(41.51840, 40.75))
+lines(c(-70.62450, -70.2), c(41.73680, 40.95))
+lines(c(-71.5, -71.13810), c(43, 42.41530))
+lines(c(-71.6, -70.66420), c(42.3, 41.95425)) # TOW1
+lines(c(-71.6, -71.77), c(42.3, 42.58)) # TOW2
+lines(c(-71.08463, -72), c(41.89072, 42.2)) # NEM
+# Plot BBH lat/lon
+points(anadromous_latlon_only$Longitude, anadromous_latlon_only$Latitude, col = 'black', pch = 21, bg = 'cadetblue3') # BBH anadromous only
+points(coors$Longitude,coors$Latitude, col = 'black', pch = 21, bg = 'cadetblue3') # BBH landlocked
+# Plot ALE lat/lon
+points(ale_base_latlon_only$Longitude, ale_base_latlon_only$Latitude, col = 'black', pch = 21, bg = 'tomato') # ALE anadromous only
+points(ale_land_latlon$Longitude, ale_land_latlon$Latitude, col = 'black', pch = 21, bg = 'tomato') # ALE landlocked
+# Both
+points(both_only$Longitude, both_only$Latitude, col = 'black', pch = 21, bg = 'slateblue2')
 
 # Add labels
 text(-65,40, expression(italic("Atlantic Ocean")), cex = 0.8)
+text(-66.0, 47.35, "MIR", cex = 0.7)
+text(-66.85, 46.4,"SJR", cex = 0.7)
+text(-62.35, 45.4,"WAU", cex = 0.7)
+text(-62.85, 44.45,"SUL", cex = 0.7)
+text(-66.7, 45.4,"DEN", cex = 0.7)
+text(-78.5, 38.5,"PAT", cex = 0.7)
+text(-74.55, 35.9,"ALL", cex = 0.7)
+text(-79.3, 36.6,"KER", cex = 0.7)
+text(-68.6, 45.73, "EGL", cex = 0.7)
+text(-74.85, 41.2, "HUD", cex =0.7)
+text(-74.2, 44.54, "LCH", cex = 0.7)
+text(-72.5, 42.7, "TOW", cex = 0.7)
+text(-72.7, 42.2, "NEM", cex = 0.7)
+
 text(-61.8, 46.8,"MAR", cex = 0.7)
 text(-65.5, 46.3,"PET", cex = 0.7)
-text(-68.25, 44.9,"EMA", cex = 0.7)
+text(-66.9, 44.4,"EMA", cex = 0.7)
 text(-70.5, 44.5,"LOC", cex = 0.7)
 text(-69.3, 43.4,"OYS", cex = 0.7)
 text(-69.3, 42.9,"EXE", cex = 0.7)
 text(-69.3, 42.4,"PAR", cex = 0.7)
-text(-72.1, 42.4,"MYS", cex = 0.7)
+text(-72.1, 43.2,"MYS", cex = 0.7)
 text(-68.8, 41.7,"HER", cex = 0.7)
-text(-70.6, 40.5,"MON", cex = 0.7)
-text(-72.2, 41.7,"GIL", cex = 0.7)
-text(-73.2, 40.2,"MET", cex = 0.7)
+text(-70.6, 40.5,"GIL", cex = 0.7)
+text(-69.4, 40.85,"MON", cex = 0.7)
+text(-73.25, 39.85,"MET", cex = 0.7)
 text(-75.7, 40.15,"DEL", cex = 0.7)
 text(-77, 39.7,"SUS", cex = 0.7)
 text(-78, 39,"POT", cex = 0.7)
 text(-77.7, 38,"RAP", cex = 0.7)
 text(-78.8, 37.55,"YOR", cex = 0.7)
 text(-78.5, 37.1,"JAM", cex = 0.7)
-text(-77.3, 36.6,"CHO", cex = 0.7)
+text(-76.75, 36.55,"CHO", cex = 0.7)
 text(-77.75, 36,"ROA", cex = 0.7)
 text(-78.2, 35.3,"NEU", cex = 0.7)
 text(-78.3, 34.6,"CF", cex = 0.7)
